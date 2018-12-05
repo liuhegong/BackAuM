0b970f76c611676f130c09a1ff387f3b1cb0e5a2
Chankin026 <350508392@qq.com>
2018-12-05 23:52:32 +0800

    数据状态

---
+++ chankin-common/src/main/java/com/chankin/util/PlatformCode.java
@@ -0,0 +1,34 @@
+package com.chankin.util;
+
+
+public enum PlatformCode {
+
+    web(1, "web"),
+    android(2, "安卓"),
+    ios(3, "IOS"),
+    other(4, "其他");
+
+    private int code;
+    private String msg;
+
+    PlatformCode(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
--- chankin-common/src/main/java/com/chankin/util/Result.java
+++ chankin-common/src/main/java/com/chankin/util/Result.java
@@ -1,8 +1,31 @@
 package com.chankin.util;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/*
+ *
+ * 响应报文结构基类
+ *
+ * */
+@ApiModel
 public class Result {
+    /**
+     * 响应码
+     */
+    @ApiModelProperty(value = "响应码", required = true, position = 0)
     private int code;
+
+    /**
+     * 响应消息
+     */
+    @ApiModelProperty(value = "响应消息", required = true, position = 1)
     private String msg;
+
+    /*
+     *  实体类
+     * */
+    @ApiModelProperty(value = "数据对象", required = false)
     private Object data = "";
 
     public Result(int code, String msg, Object data) {
